958766ab93e531836dbe8d333f98d716aab16b4a
sujitha <sujipinku07@gmail.com>
2024-12-16 13:54:14 +0530

    first

--- src/components/CellContainer.js
+++ src/components/CellContainer.js
@@ -3,10 +3,9 @@ function CellsContainer({ numbers,onClick }) {
     return ( 
       <div>
         {numbers.map((number, index) => (
-          <div key={index} onClick={() => onClick(index)}>
+          <span key={index} onClick={() => onClick(index)}>
             {number}
-            {number}
-          </div>
+          </span>
         ))}
       </div>
     );
