4a5ef13a397d9dda19087417ea7a64e24f6301fd
sujitha <sujipinku07@gmail.com>
2024-12-16 14:00:22 +0530

    first

--- src/components/CellContainer.js
+++ src/components/CellContainer.js
@@ -1,13 +1,23 @@
 import React from 'react';
 function CellsContainer({ numbers,onClick }) {
+  //Determine the number of cells per row based on the level of cells
+const cellsPerRow = Math.sqrt(numbers.length);
+const rows = [];
+for (let i = 0; i < numbers.length; i += cellsPerRow) {
+rows.push(numbers.slice(i, i + cellsPerRow));
+}
     return ( 
       <div>
-        {numbers.map((number, index) => (
-          <span key={index} onClick={() => onClick(index)}>
-            {number}
-          </span>
-        ))}
-      </div>
+       {rows.map((row, rowIndex) => (
+                <div key={rowIndex}>
+                    {row.map((number, index) => (
+                        <span key={index} onClick={() => onClick(rowIndex * cellsPerRow + index)}>
+                            {number}
+                        </span>
+                    ))}
+                </div>
+            ))}
+        </div>
     );
-  }
-  export default CellsContainer;
+}
+export default CellsContainer;
