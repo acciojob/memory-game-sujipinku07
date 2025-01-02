078c4bee5fd868ece57d6ec5cd7d9922e05b0048
sujitha <sujipinku07@gmail.com>
2025-01-02 08:32:06 +0530

    memory-game

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Game from "./components/Game";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/index";
+import MemoryGameContainer from "./container/MemoryGameContainer/MemoryGameContainer";
 
+const store = createStore(rootReducer);
 
+function App() {
+  return (
+    <Provider store={store}>
+      <MemoryGameContainer />
+    </Provider>
+  );
+}
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
